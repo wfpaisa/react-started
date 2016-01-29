e5e33c2a7c750d2307b0400da303918685337a0b
Felipe Uribe <wfpaisa@gmail.com>
2016-01-29 17:43:22 -0500

    lista de tareas

--- app/main.js
+++ app/main.js
@@ -1,16 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var Lista = React.createClass({
-	render: function(){
-		var itemLi = function(){
-			return<li>uno</li>
-		}
+//clase que crea la lista de tareas
+var TodoList = React.createClass({
+    render: function(){
+    		self = this
+        var createItem = function(itemText, index){
 
-		return <ul>{itemLi()}</ul>
-	}
+          return <li key={index}>{itemText} <span onClick={self.props.hclick.bind(null,index)}>x</span></li>
+
+        }
+
+        return <ul>{this.props.items.map(createItem)}</ul>
+    }
 })
 
-ReactDOM.render( < Lista / > ,
+//clase que crea el formulario y lo muestra junto con la lista de tareas
+var FormApp = React.createClass({
+    getInitialState: function()
+    {
+        return {
+            text: '',
+            items: ["a","b","c","d"]
+        };
+    },
+    onChange: function(e)
+    {
+        this.setState({text: e.target.value});
+    },
+    removerElemento: function(index){
+
+    	this.setState({
+    		items : React.addons.update(this.state.items, {$splice: [[index, 1]]})
+    	})
+
+    },
+    handleSubmit: function(e)
+    {
+        e.preventDefault();
+        var nextItems = this.state.items.concat([this.state.text]);
+        var nextText = '';
+        this.setState({items: nextItems, text: nextText});
+    },
+    render: function()
+    {
+        return (
+            <div>
+                <h3>React Todo List</h3>
+                <TodoList items={this.state.items} hclick={this.removerElemento}/>
+                <form onSubmit={this.handleSubmit}>
+                  <input type="text" onChange={this.onChange} value={this.state.text} />
+                  <button type="submit">Add</button>
+                </form>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render( <FormApp / > ,
   document.getElementById('app')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var node_modules = path.resolve(__dirname, 'node_modules');
-var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
+var pathToReact = path.resolve(node_modules, 'react/dist/react-with-addons.min');
 var pathToReactDOM = path.resolve(node_modules, 'react-dom/dist/react-dom.min.js');
 
 config = {
